0de56974b0a3b59d25d2582243723dc4c248036d
Ellis <ellischung98@gmail.com>
2023-09-06 19:18:25 -0400

    finished valid sudoku

---
+++ arrays_hashing/valid_sudoku.py
@@ -0,0 +1,44 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+# Note:
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        square = defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[(r // 3, c // 3)]:
+                    return False
+                else:
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    square[(r // 3, c // 3)].add(board[r][c])
+
+
+
+
+        return True
+
+NewSolution = Solution()
+print(NewSolution.isValidSudoku([["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))

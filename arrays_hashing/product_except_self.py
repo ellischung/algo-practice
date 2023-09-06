4c5b9b87ac11f4076bd1eedc268d90b8bf806be8
Ellis <ellischung98@gmail.com>
2023-09-06 18:24:54 -0400

    finished product except self

---
+++ arrays_hashing/product_except_self.py
@@ -0,0 +1,52 @@
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+# Example 1:
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+
+# Example 2:
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+
+# Constraints:
+# 2 <= nums.length <= 105
+# -30 <= nums[i] <= 30
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+# Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+from typing import List
+
+class Solution:
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     product = 1
+
+    #     for n in nums:
+    #         product *= n
+        
+    #     res = []
+    #     for i in range(len(nums)):
+    #         res.append(product // nums[i])
+    #     # for n in nums:
+    #     #     res.append(product // n)
+
+    #     return res
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums) 
+
+        leftProduct = 1
+        for i in range(len(nums)):
+            ans[i] = leftProduct
+            leftProduct *= nums[i]
+
+        rightProduct = 1
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] *= rightProduct
+            rightProduct *= nums[i]
+
+        return ans
+    
+new_solution = Solution()
+print(new_solution.productExceptSelf([1,2,3,4]))
